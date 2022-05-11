2b12dd44d9eb36b33360db6a5e173da5cb90a54b
Irsyad <irsyadrbbn19@gmail.com>
2022-05-11 21:42:01 +0700

    Upload to Repo

---
+++ client/src/components/Announcement.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import { mobile } from '../responsive'
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+    ${mobile({ fontSize: "12px"})}
+`
+const Announcement = () => {
+  return (
+    <Container>
+        Summer Sale! Free Shipping on Orders over $50
+    </Container>
+  )
+}
+
+export default Announcement
---
+++ client/src/data.js
@@ -0,0 +1,80 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "./assets/mens-2.png",
+        title: "T-SHIRT SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF FROM NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/zZ6DWDG/business-suit-woman.png",
+        title: "FORMAL SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF FROM NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "./assets/mens-3.png",
+        title: "JACKET SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF FROM NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+
+];
+
+export const categories = [
+    {
+        id:1,
+        img: "./assets/categories-3.jpeg",
+        title: "T-SHIRT STYLE",
+        cat: "tshirt"
+    },
+    {
+        id:2,
+        img: "./assets/categories-1.jpeg",
+        title: "SUIT & DRESS",
+        cat: "formal"
+    },
+    {
+        id:3,
+        img: "./assets/categories-2.jpeg",
+        title: "JACKETS",
+        cat: "jacket"
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "./assets/Tshirt-White.jpg",
+    },
+    {
+        id: 2,
+        img: "./assets/Tshirt-Black.jpg",
+    },
+    {
+        id: 3,
+        img: "./assets/KP-white.jpg",
+    },
+    {
+        id: 4,
+        img: "./assets/KP-black.jpg",
+    },
+    {
+        id: 5,
+        img: "./assets/Kemeja-White.jpg",
+    },
+    {
+        id: 6,
+        img: "./assets/Kemeja-Black.jpg",
+    },
+    {
+        id: 7,
+        img: "./assets/Hoodie-Cream.jpg",
+    },
+    {
+        id: 8,
+        img: "./assets/Hoodie-Blue.jpg",
+    },
+];
